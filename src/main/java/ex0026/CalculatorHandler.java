3f8bb5f9c5c16e267826d633316a280f17468849
Florian <florian.thuswohl@aon.at>
2022-05-19 20:09:45 +0200

    changes

--- src/main/java/ex0026/CalculatorHandler.java
+++ src/main/java/ex0026/CalculatorHandler.java
@@ -1,10 +1,68 @@
 package ex0026;
 
+import java.io.*;
 import java.net.Socket;
+import java.util.Arrays;
+import java.util.Random;
 
 public class CalculatorHandler extends AbstractHandler {
     @Override
     protected void runTask(Socket socketClient) {
+        try {
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socketClient.getOutputStream()));
+            BufferedReader br = new BufferedReader(new InputStreamReader(socketClient.getInputStream()));
 
+            String command = br.readLine();
+            String tokens[] = command.split(";");
+
+            if (tokens.length == 3) {
+                int n = Integer.parseInt(tokens[0]);
+                int min = Integer.parseInt(tokens[1]);
+                int max = Integer.parseInt(tokens[2]);
+
+                Random r = new Random();
+                int numbers[] = new int[n];
+
+                for (int i = 0; i < n; i++) {
+                    numbers[i] = min + r.nextInt(max - min + 1);
+                }
+                Arrays.sort(numbers);
+                int k, i;
+                print(numbers, n);
+                i = 0;
+                while (i < n - 1) {
+                    if (numbers[i] == numbers[i + 1]) {
+                        for (k = i; k < n - 1; k++) {
+                            numbers[k] = numbers[k + 1];
+                        }
+                        n--;
+                    } else {
+                        i++;
+                    }
+                }
+                print(numbers, n);
+                StringBuilder sb = new StringBuilder();
+                for (i = 0; i < n - 1; i++) {
+                    sb.append(numbers[i]);
+                    sb.append(";");
+                }
+                sb.append(numbers[n - 1]);
+                bw.write(sb.toString());
+                bw.flush();
+            }
+
+            br.close();
+            bw.close();
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
+
+    private void print(int numbers[], int n) {
+        int x;
+        for (x = 0; x < n; x++) {
+            System.out.print(numbers[x] + " ");
+        }
+        System.out.println();
     }
 }
--- src/main/java/ex0026/Client.java
+++ src/main/java/ex0026/Client.java
@@ -1,7 +1,36 @@
 package ex0026;
 
-public class Client {
-    public static void main(String[] args) {
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Scanner;
 
+public class Client {
+    public static void main(String[] args) throws IOException {
+        final InetAddress IP = InetAddress.getByName(args[0]);
+        final int PORT = Integer.parseInt(args[1]);
+
+        Socket socket = new Socket(IP, PORT);
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        Scanner scanner = new Scanner(System.in);
+
+        String input;
+        do {
+            System.out.print("Input (count;min;max): ");
+            input = scanner.nextLine();
+
+            writer.write(input + System.lineSeparator());
+            writer.flush();
+
+            String[] result = reader.readLine().split(";");
+            int[] numbers = new int[result.length];
+
+            for (int i = 0; i < result.length; i++) {
+                numbers[i] = Integer.parseInt(result[i]);
+                System.out.println(String.format("%-3s| %-3s", i + 1, numbers[i]));
+            }
+        } while (!input.equals("exit"));
     }
 }
